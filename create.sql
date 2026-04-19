ee1e7c3bdce865740e92e6b368fc2867b6941305
Jose Beltran <jmbg7318@gmail.com>
2026-04-18 20:14:44 -0500

    Reworked SQL Table Creation

--- create.sql
+++ create.sql
@@ -1,198 +1,128 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 31, 2026 at 03:51 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Creating Tables
+CREATE TABLE users (
+    userID INT NOT NULL,
+    FName VARCHAR(50) NOT NULL,
+    LName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    Role ENUM('user','admin') DEFAULT 'user',
+    Genre1 VARCHAR(100),
+    Genre2 VARCHAR(100),
+    Genre3 VARCHAR(100)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE author (
+    authorID INT NOT NULL,
+    authorName VARCHAR(100) NOT NULL
+);
 
+CREATE TABLE books (
+    mmsID VARCHAR(20) NOT NULL,
+    Title VARCHAR(150) NOT NULL,
+    callNumber VARCHAR(50) NOT NULL,
+    authorID INT NOT NULL
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE books_copy (
+    Barcode VARCHAR(30) NOT NULL,
+    mmsID VARCHAR(20) NOT NULL,
+    status VARCHAR(30) NOT NULL
+);
 
---
--- Database: `create`
---
+CREATE TABLE genres (
+    genreID INT NOT NULL,
+    genreName VARCHAR(50) NOT NULL
+);
 
--- --------------------------------------------------------
+CREATE TABLE book_genre (
+    mmsID VARCHAR(20) NOT NULL,
+    genreID INT NOT NULL
+);
 
---
--- Table structure for table `author`
---
+CREATE TABLE prefers (
+    userID INT NOT NULL,
+    genreID INT NOT NULL
+);
 
-CREATE TABLE `author` (
-  `authorID` int(11) NOT NULL,
-  `authorName` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE is_recommended (
+    recID INT NOT NULL,
+    userID INT NOT NULL,
+    mmsID VARCHAR(20) NOT NULL
+);
 
--- --------------------------------------------------------
+CREATE TABLE book_log (
+    logID INT NOT NULL,
+    mmsID VARCHAR(20) NOT NULL,
+    Action VARCHAR(50) NOT NULL,
+    performedBy INT NOT NULL,
+    timeStamp DATETIME NOT NULL
+);
 
---
--- Table structure for table `books`
---
+-- Constraint for PRIMARY KEYS
+ALTER TABLE users
+ADD CONSTRAINT pk_users PRIMARY KEY (userID);
 
-CREATE TABLE `books` (
-  `mmsID` varchar(20) NOT NULL,
-  `authorID` int(11) NOT NULL,
-  `title` varchar(150) NOT NULL,
-  `callNumber` varchar(50) NOT NULL,
-  `barcode` varchar(20) NOT NULL,
-  `inventory` int(11) DEFAULT 0,
-  `borrowDate` date DEFAULT NULL,
-  `returnDate` date DEFAULT NULL,
-  `numOfLoans` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE author
+ADD CONSTRAINT pk_author PRIMARY KEY (authorID);
 
--- --------------------------------------------------------
+ALTER TABLE books
+ADD CONSTRAINT pk_books PRIMARY KEY (mmsID);
 
---
--- Table structure for table `book_genre`
---
+ALTER TABLE books_copy
+ADD CONSTRAINT pk_books_copy PRIMARY KEY (Barcode);
 
-CREATE TABLE `book_genre` (
-  `mmsID` varchar(20) NOT NULL,
-  `genreID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE genres
+ADD CONSTRAINT pk_genres PRIMARY KEY (genreID);
 
--- --------------------------------------------------------
+ALTER TABLE book_genre
+ADD CONSTRAINT pk_book_genre PRIMARY KEY (mmsID, genreID);
 
---
--- Table structure for table `genres`
---
+ALTER TABLE prefers
+ADD CONSTRAINT pk_prefers PRIMARY KEY (userID, genreID);
 
-CREATE TABLE `genres` (
-  `genreID` int(11) NOT NULL,
-  `genreName` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE is_recommended
+ADD CONSTRAINT pk_is_recommended PRIMARY KEY (recID);
 
--- --------------------------------------------------------
+ALTER TABLE book_log
+ADD CONSTRAINT pk_book_log PRIMARY KEY (logID);
 
---
--- Table structure for table `is_recommended`
---
+-- Constraint for FOREIGN KEYS
+ALTER TABLE books
+ADD CONSTRAINT fk_books_author
+FOREIGN KEY (authorID) REFERENCES author(authorID);
 
-CREATE TABLE `is_recommended` (
-  `recID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `mmsID` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE books_copy
+ADD CONSTRAINT fk_copy_books
+FOREIGN KEY (mmsID) REFERENCES books(mmsID);
 
--- --------------------------------------------------------
+ALTER TABLE book_genre
+ADD CONSTRAINT fk_bookgenre_books
+FOREIGN KEY (mmsID) REFERENCES books(mmsID);
 
---
--- Table structure for table `prefers`
---
+ALTER TABLE book_genre
+ADD CONSTRAINT fk_bookgenre_genres
+FOREIGN KEY (genreID) REFERENCES genres(genreID);
 
-CREATE TABLE `prefers` (
-  `userID` int(11) NOT NULL,
-  `genreID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE prefers
+ADD CONSTRAINT fk_prefers_users
+FOREIGN KEY (userID) REFERENCES users(userID);
 
--- --------------------------------------------------------
+ALTER TABLE prefers
+ADD CONSTRAINT fk_prefers_genres
+FOREIGN KEY (genreID) REFERENCES genres(genreID);
 
---
--- Table structure for table `users`
---
+ALTER TABLE is_recommended
+ADD CONSTRAINT fk_recommended_users
+FOREIGN KEY (userID) REFERENCES users(userID);
 
-CREATE TABLE `users` (
-  `userID` int(11) NOT NULL,
-  `Fname` varchar(50) NOT NULL,
-  `Lname` varchar(50) NOT NULL,
-  `Email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE is_recommended
+ADD CONSTRAINT fk_recommended_books
+FOREIGN KEY (mmsID) REFERENCES books(mmsID);
 
---
--- Indexes for dumped tables
---
+ALTER TABLE book_log
+ADD CONSTRAINT fk_log_books
+FOREIGN KEY (mmsID) REFERENCES books(mmsID);
 
---
--- Indexes for table `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`authorID`);
-
---
--- Indexes for table `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`mmsID`),
-  ADD UNIQUE KEY `BOOKMMSID` (`mmsID`),
-  ADD UNIQUE KEY `BOOKBARCODE` (`barcode`),
-  ADD KEY `authorID` (`authorID`);
-
---
--- Indexes for table `book_genre`
---
-ALTER TABLE `book_genre`
-  ADD PRIMARY KEY (`mmsID`,`genreID`),
-  ADD KEY `genreID` (`genreID`);
-
---
--- Indexes for table `genres`
---
-ALTER TABLE `genres`
-  ADD PRIMARY KEY (`genreID`);
-
---
--- Indexes for table `is_recommended`
---
-ALTER TABLE `is_recommended`
-  ADD PRIMARY KEY (`recID`),
-  ADD KEY `userID` (`userID`),
-  ADD KEY `mmsID` (`mmsID`);
-
---
--- Indexes for table `prefers`
---
-ALTER TABLE `prefers`
-  ADD PRIMARY KEY (`userID`,`genreID`),
-  ADD KEY `genreID` (`genreID`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `books`
---
-ALTER TABLE `books`
-  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`authorID`) REFERENCES `author` (`authorID`) ON UPDATE CASCADE;
-
---
--- Constraints for table `book_genre`
---
-ALTER TABLE `book_genre`
-  ADD CONSTRAINT `book_genre_ibfk_1` FOREIGN KEY (`mmsID`) REFERENCES `books` (`mmsID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `book_genre_ibfk_2` FOREIGN KEY (`genreID`) REFERENCES `genres` (`genreID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `is_recommended`
---
-ALTER TABLE `is_recommended`
-  ADD CONSTRAINT `is_recommended_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `is_recommended_ibfk_2` FOREIGN KEY (`mmsID`) REFERENCES `books` (`mmsID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `prefers`
---
-ALTER TABLE `prefers`
-  ADD CONSTRAINT `prefers_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `prefers_ibfk_2` FOREIGN KEY (`genreID`) REFERENCES `genres` (`genreID`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE book_log
+ADD CONSTRAINT fk_log_users
+FOREIGN KEY (PerformedBy) REFERENCES users(userID);
